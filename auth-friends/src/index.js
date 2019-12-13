6b0efe95254cab855a7b15712bb3c846af1c6406
Tyree Lebeau <tyreelebeau@Tyrees-iMac.local>
2019-12-12 22:25:43 -0600

    added reducer and actions and installed some dependices

--- auth-friends/src/index.js
+++ auth-friends/src/index.js
@@ -2,7 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux'
+import thunk from 'redux-thunk'
+import { BrowserRouter as Router } from 'react-router-dom'
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
