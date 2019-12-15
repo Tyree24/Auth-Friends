a3969c698d244a6a7d4f2e07f570f63b5cf7f6bc
Tyree Lebeau <tyreelebeau@Tyrees-iMac.local>
2019-12-15 16:39:29 -0600

    added form and han dlers and updated some css

--- auth-friends/src/components/Login.js
+++ auth-friends/src/components/Login.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { axiosWithAuth } from "../utils/axiosWithAuth";
 
 const loginStyles = {
     display: "flex",
@@ -12,11 +13,55 @@ function Login() {
     const [credentials, setCredentials] = useState({});
     const [isLoading, setIsLoading] = useState(false);
 
+    const handleChanges = e => {
+        setCredentials({
+            ...credentials,
+            [e.target.name]: e.target.value
+        });
+    };
+
+    const handleSubmit = e => {
+        e.preventDefault();
+        setIsLoading(true);
+
+        axiosWithAuth()
+            .post("/login", credentials)
+            .then(res => {
+                console.log(res);
+            })
+            .catch(err => console.log(err));
+    };
+
+
     if (isLoading) return <div className="loader">Loading...</div>;
 
     return (
         <div className="login" style={loginStyles}>
             <h2>Login</h2>
+            <form className="form" onSubmit={handleSubmit}>
+                <label htmlFor="username" hidden>
+                    Username:
+        </label>
+                <input
+                    name="username"
+                    type="text"
+                    placeholder="Username"
+                    value={credentials.username}
+                    onChange={handleChanges}
+                />
+                <label htmlFor="password" hidden>
+                    Password:
+        </label>
+                <input
+                    name="password"
+                    type="password"
+                    placeholder="Password"
+                    value={credentials.password}
+                    onChange={handleChanges}
+                />
+                <button>Login</button>
+            </form>
+
         </div>
     );
 }
--- auth-friends/src/index.js
+++ auth-friends/src/index.js
@@ -2,11 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-
-import { createStore, applyMiddleware } from 'redux';
-import { Provider } from 'react-redux'
-import thunk from 'redux-thunk'
-import { BrowserRouter as Router } from 'react-router-dom'
+import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
